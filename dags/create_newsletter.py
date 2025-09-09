1c5d0b52a1c4645f1061caa24131b9b4bcaf42d3
Vikramaditya Tatke <vikramadityatatke@proton.me>
2025-09-10 00:54:04 +0100

    Update: Sync latest changes

--- dags/create_newsletter.py
+++ dags/create_newsletter.py
@@ -4,7 +4,7 @@ import os
 from airflow.sdk import asset
 
 
-# set these enviroment variables in order to store the newsletter 
+# set these enviroment variables in order to store the newsletter
 # in cloud object storage instead of the local filesystem
 OBJECT_STORAGE_SYSTEM = os.getenv("OBJECT_STORAGE_SYSTEM", default="file")
 OBJECT_STORAGE_CONN_ID = os.getenv("OBJECT_STORAGE_CONN_ID", default=None)
@@ -62,3 +62,43 @@ def selected_quotes(context: dict) -> dict:
         "long_q": long_quote,
     }
 
+
+@asset(schedule=[selected_quotes])
+def format_newsletter(context):
+    """
+    Formats the newsletter.
+    """
+    from airflow.io.path import ObjectStoragePath
+
+    object_storage_path = ObjectStoragePath(
+        f"{OBJECT_STORAGE_SYSTEM}://{OBJECT_STORAGE_PATH_NEWSLETTER}",
+        conn_id=OBJECT_STORAGE_CONN_ID,
+    )
+
+    date = context["dag_run"].run_after.strftime("%Y-%m-%d")
+
+    selected_quotes = context["ti"].xcom_pull(
+        dag_id="selected_quotes",
+        task_ids=["selected_quotes"],
+        key="return_value",
+        include_prior_dates=True,
+    )
+    logger.info("Before selected_quotes %s", selected_quotes)
+
+    newsletter_template_path = object_storage_path / "newsletter_template.txt"
+
+    newsletter_template = newsletter_template_path.read_text()
+
+    newsletter = newsletter_template.format(
+        quote_text_1=selected_quotes["short_q"]["q"],
+        quote_author_1=selected_quotes["short_q"]["a"],
+        quote_text_2=selected_quotes["median_q"]["q"],
+        quote_author_2=selected_quotes["median_q"]["a"],
+        quote_text_3=selected_quotes["long_q"]["q"],
+        quote_author_3=selected_quotes["long_q"]["a"],
+        date=date,
+    )
+
+    date_newsletter_path = object_storage_path / f"{date}_newsletter.txt"
+
+    date_newsletter_path.write_text(newsletter)
